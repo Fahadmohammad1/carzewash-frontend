832af1af2ca590ded301e0177b11538fb6aed2f8
Fahad <205fahadmohammad@gmail.com>
2024-12-03 20:12:16 +0600

    booking other all field add

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -1,18 +1,16 @@
 import React from "react";
+import user from "../../assets/bookingForm/user.png";
+import phone from "../../assets/bookingForm/phone.png";
+import car from "../../assets/bookingForm/car.png";
+import date from "../../assets/bookingForm/date.png";
+import clock from "../../assets/bookingForm/clock.png";
 
 const BookingForm = () => {
   return (
     <section className="mt-[26px] border-[2px] rounded-xl">
-      <div className="px-5 py-8">
-        <label className="flex items-center gap-2 rounded-[10px] border-[1px] py-3 px-[18px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70"
-          >
-            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-          </svg>
+      <div className="px-5 py-8 flex flex-col gap-y-3">
+        <label className="flex items-center gap-[10px] rounded-[10px] border-[1px] py-3 px-[18px] ">
+          <img src={user} alt="" />
           <input
             type="text"
             className="border-0"
@@ -21,6 +19,59 @@ const BookingForm = () => {
             placeholder="Votre nom complet"
           />
         </label>
+        <label className="flex items-center gap-[10px] rounded-[10px] border-[1px] py-3 px-[18px]">
+          <img src={phone} alt="" />
+          <input
+            type="text"
+            className="border-0"
+            name="phone"
+            id="phone"
+            placeholder="téléphone"
+          />
+        </label>
+        <label className="flex items-center gap-[10px] rounded-[10px] border-[1px] py-3 px-[18px]">
+          <img src={car} alt="" />
+          <input
+            type="text"
+            className="border-0"
+            name="car"
+            id="car"
+            placeholder="Car Model"
+          />
+        </label>
+        <div className="flex justify-between">
+          <label className="flex items-center gap-[10px] rounded-[10px] border py-3 px-[18px] w-[134px]">
+            <img src={date} className="" alt="" />
+            <input
+              type="text"
+              className="border-0 w-[73px]"
+              name="car"
+              id="car"
+              placeholder="Car Model"
+            />
+          </label>
+          <label className="flex items-center gap-[10px] rounded-[10px] border py-3 px-[18px] w-[134px]">
+            <img src={clock} className="" alt="" />
+            <input
+              type="text"
+              className="border-0 w-[73px]"
+              name="car"
+              id="car"
+              placeholder="Car Model"
+            />
+          </label>
+        </div>
+        <select className="px-[18px] py-5 bg-white border rounded-[10px] text-gray-400 mb-3">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+        <p className="mb-8 text-gray-400 italic">
+          Vous serez contacté dans les plus brefs délais
+        </p>
+        <button className="px-[51px] py-[17px] bg-cw-primary rounded-md">
+          Réservation
+        </button>
       </div>
     </section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/home/Banner";
 import BookingForm from "../components/home/BookingForm";
+import Services from "../components/home/Services";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <BookingForm />
+      <Services />
     </>
   );
 };

e876ca06d043f7f850ef8c7b1b4bad2e49319a79
Fahad <205fahadmohammad@gmail.com>
2024-12-03 20:34:02 +0600

    service section add

--- src/components/home/Service.jsx
+++ src/components/home/Service.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import service1 from "../../assets/service/service-1.png";
 
 const Service = () => {
-  return <div></div>;
+  return (
+    <div className="px-[13px] py-[18px] rounded-[20px] shadow-md">
+      <img className="rounded-xl w-full mb-5" src={service1} alt="car wash" />
+      <div>
+        <h3 className="text-[28px] leading-[40px] font-extrabold mb-2 text-cw-secondary">
+          Services Professionnels
+        </h3>
+        <p className="text-base leading-6">
+          Notre équipe expérimentée utilise des techniques avancées pour un
+          nettoyage méticuleux.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Service from "./Service";
 
 const Services = () => {
-  return <div></div>;
+  return (
+    <section className="mt-[60px]">
+      <h1 className="text-[28px] leading-[42px] font-black text-center mb-3">
+        <span className="text-cw-secondary">Apportant de l’éclat</span>{" "}
+        <span className="text-cw-primary">pour votre voiture</span>
+      </h1>
+      <p className="text-base leading-6 text-center mb-12">
+        Nous offrons des services de nettoyage et d’entretien conçus pour
+        restaurer l’apparence et la propreté de votre véhicule, en mettant
+        l’accent sur la qualité et la satisfaction du client.
+      </p>
+
+      <div>
+        <Service />
+      </div>
+    </section>
+  );
 };
 
 export default Services;

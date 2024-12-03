6005c0c2af9b6b900d6522dcf83b6a37894ff8db
Fahad <205fahadmohammad@gmail.com>
2024-12-03 23:11:30 +0600

    service section cmplt

--- src/components/home/Service.jsx
+++ src/components/home/Service.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import service1 from "../../assets/service/service-1.png";
 
-const Service = () => {
+const Service = ({ service }) => {
   return (
-    <div className="px-[13px] py-[18px] rounded-[20px] shadow-md">
-      <img className="rounded-xl w-full mb-5" src={service1} alt="car wash" />
+    <div
+      className={`px-[13px] py-[18px] rounded-[20px] shadow-md ${service.bgColor}`}
+    >
+      <img
+        className="rounded-xl w-full mb-5"
+        src={service.image}
+        alt="car wash"
+      />
       <div>
         <h3 className="text-[28px] leading-[40px] font-extrabold mb-2 text-cw-secondary">
-          Services Professionnels
+          {service.title}
         </h3>
-        <p className="text-base leading-6">
-          Notre équipe expérimentée utilise des techniques avancées pour un
-          nettoyage méticuleux.
-        </p>
+        <p className="text-base leading-6">{service.description}</p>
       </div>
     </div>
   );
--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import Service from "./Service";
+import service1 from "../../assets/service/service-1.png";
+import service2 from "../../assets/service/service-2.png";
+import service3 from "../../assets/service/service-3.png";
 
 const Services = () => {
+  const services = [
+    {
+      id: 1,
+      image: service1,
+      bgColor: "bg-white",
+      title: "Services Professionnels",
+      description:
+        "Notre équipe expérimentée utilise des techniques avancées pour un nettoyage méticuleux.",
+    },
+    {
+      id: 2,
+      image: service2,
+      bgColor: "bg-cw-primary",
+      title: "Équipements haut de gamme",
+      description:
+        "Nous utilisons les meilleurs outils pour garantir un lavage impeccable et sécurisé.",
+    },
+    {
+      id: 3,
+      image: service3,
+      bgColor: "bg-white",
+      title: "Tarifs Compétitifs",
+      description:
+        "Des services de qualité à des prix abordables pour tous nos clients.",
+    },
+  ];
   return (
     <section className="mt-[60px]">
       <h1 className="text-[28px] leading-[42px] font-black text-center mb-3">
@@ -14,8 +43,10 @@ const Services = () => {
         l’accent sur la qualité et la satisfaction du client.
       </p>
 
-      <div>
-        <Service />
+      <div className="grid grid-cols-1 gap-y-8">
+        {services?.map((service) => (
+          <Service key={service.id} service={service} />
+        ))}
       </div>
     </section>
   );
